11ecd84f5bddd21135fa7fbfea82a7db665e7c7c
Geoffrey Gichaga <geoffrey.githinji@student.moringaschool.com>
2022-04-11 09:53:44 +0300

    Arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1=62,num2=1;
+const multiply= num1*num2
+
+let random = Math.floor(Math.random()>0)
+
+const num3 = 24,num4=5
+let mod = num3%num4
+
+let max = Math.max(1,5,0,8,4,20,8,17)
+
+
